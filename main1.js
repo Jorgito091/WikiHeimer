0cac9f9e2e6301cdfd41df927e4208cc4451a87d
Jorsh <hogojorsj@gmail.com>
2025-03-13 20:06:49 -0700

    Cambios

---
+++ main1.js
@@ -0,0 +1,3 @@
+// Este archivo puede estar vacío por ahora si no necesitas JavaScript en esta página,
+// pero puedes añadir funcionalidades interactivas más adelante.
+console.log("Página cargada con éxito.");
